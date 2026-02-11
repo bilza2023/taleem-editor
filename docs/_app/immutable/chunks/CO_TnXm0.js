5fe19c2068b0fbd9e1fd94a5a6d8323c99c161b6
bilza2023 <bilza2023@gmail.com>
2026-02-11 17:22:44 +0500

    test-based used in player

---
+++ docs/_app/immutable/chunks/CO_TnXm0.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_22momq?.base??"/taleem-editor",e=globalThis.__sveltekit_22momq?.assets??s??"";export{e as a,s as b};
---
+++ docs/_app/immutable/entry/start.BVt9pM6v.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/B_pIY2z0.js";export{o as load_css,r as start};
